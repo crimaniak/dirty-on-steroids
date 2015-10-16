da4e7399b7b60d6b112ff3ea923673ae9bf65a7b
Pavel <pavel.sokolov@gmail.com>
2015-10-16 17:08:42 +0300

    Update gender.color.js

--- modules/gender.color.js
+++ modules/gender.color.js
@@ -11,7 +11,7 @@ d3.addModule(
 	},
 
 	onComment: function(comment){
-		this.processElements(comment.getFooter().get(0).querySelectorAll('a.c_user'));
+		this.processElementsComment(comment.getFooter().get(0).querySelectorAll('a.c_user'));
 	},
 
 	//expecting array of a.c_user DOM elements
@@ -32,5 +32,36 @@ d3.addModule(
 			elemArray[i].style.textDecoration = 'none';
 		}
 	},
+	//expecting array of a.c_user DOM elements for comments
+	processElementsComment: function(elemArray){
+		if(!elemArray)return;
+		for(var i = 0; i < elemArray.length; i++)
+		{
+			iconAnchor = elemArray[i].parentNode.querySelector('a.c_icon');
+			if ( iconAnchor )
+			{
+				var nextNode = iconAnchor.nextSibling;
+				while( nextNode )
+				{
+					if ( nextNode.nodeName == 'SPAN' )
+					{
+						if ( nextNode.innerHTML.indexOf('Написала') > -1 )
+						{
+							//female
+							elemArray[i].style.borderBottom = '1px solid #ff78f7';
+						}
+						else
+						{
+							//male
+							elemArray[i].style.borderBottom = '1px solid #5086ff';
+						}
+						elemArray[i].style.textDecoration = 'none';
+						break;
+					}
+					nextNode = nextNode.nextSibling;
+				}
+			}
+		}
+	},	
 });
 	
